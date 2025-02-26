e33c3d178005fd3bc93cbd9ff80e62e16b2c2134
Mayank <mayankghatpande10@gmail.com>
2025-02-26 23:40:32 +0530

    Added pagination

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -6,13 +6,20 @@ function PokemonList()
 {
   const [pokemonList,setPokemonList] = useState([]);
   const [isLoading,setIsLoading] = useState(true);
-  const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon';
+  const [pokedexUrl,setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+
+  const [nextUrl,setNextUrl] = useState('');
+  const [prevUrl,setPrevUrl] = useState('');
   async function downloadPokemons()
   {
-    const response = await axios.get(POKEDEX_URL); // this downloads the list of 20 pokemons
+    setIsLoading(true);
+    const response = await axios.get(pokedexUrl); // this downloads the list of 20 pokemons
     const pokemonResults = response.data.results; // we get the array of pokemons from result
     console.log(pokemonResults);
-    // iterating over the array of pokemons , and using their url , to create an array of promises 
+    // iterating over the array of pokemons , and using their url , to create an array of promises
+    console.log(response.data);
+    setNextUrl(response.data.next);
+    setPrevUrl(response.data.previous); 
     // that will download those 20 pokemons
     const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
 
@@ -36,7 +43,7 @@ function PokemonList()
   }
   useEffect(()=>{
    downloadPokemons();
-  },[]);
+  },[pokedexUrl]);
  
   return (
   <>
@@ -45,8 +52,8 @@ function PokemonList()
      {(isLoading) ? 'Loading...' : pokemonList.map((p)=> <Pokemon name ={p.name} image={p.image} key={p.id}/>)}
      </div>
      <div className="controls">
-      <button>Prev</button>
-      <button>Next</button>
+      <button disabled={prevUrl == undefined} onClick={()=> setPokedexUrl(prevUrl)}>Prev</button>
+      <button disabled={nextUrl == undefined} onClick={()=> setPokedexUrl(nextUrl)}>Next</button>
 
      </div>
    </div>
